a48c1c50ecc1280148129bb1201635fbc821550b
Carlos Ivan Mercado Marin <karlos123jun@gmail.com>
2022-03-05 16:00:56 -0600

    modulo usuario inciado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EliminarusuarioComponent } from './components/user/eliminarusuario/eliminarusuario.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ModificarcontrasenyaComponent } from './components/user/modificarcontrasenya/modificarcontrasenya.component'
 
 const routes: Routes = [
-  {path: '' , redirectTo: '/auth',pathMatch: 'full'},
-  {path: 'auth', loadChildren: () => import ('./auth/auth.module').then(x => x.AuthModule) }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'auth', loadChildren: () => import ('./auth/auth.module').then(x => x.AuthModule) },
+  { path: 'deleteuser',component: EliminarusuarioComponent,pathMatch: 'full' },
+  { path: 'changepass',component: ModificarcontrasenyaComponent,pathMatch:'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/models/userpass.ts
@@ -0,0 +1,6 @@
+export interface UserP {
+  IDusuario: number,
+  passold: string,
+  passnew: string,
+  passconf: string
+}
