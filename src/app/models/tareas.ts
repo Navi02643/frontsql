811ccdf1e06d800921d44a1037e26692933f5f69
AngelaMTC <tc.angela.m@gmail.com>
2022-03-17 16:12:25 -0600

    Changes on Module Tareas(16/03/22).

--- src/app/models/tareas.ts
+++ src/app/models/tareas.ts
@@ -1,7 +1,17 @@
+// MODEL TAREAS:
 export interface tareas {
-        IDtarea: number,
-        nombre: string,
-        descripcion: string,
-        proyecto: string,
-        fecha_finalizacion: string,
-    }
+        // IDtarea: number,
+        // nombre: string,
+        // descripcion: string,
+        // proyecto: string,
+        // fecha_finalizacion: string,
+    
+    IDtareas: number,
+    IDproyecto: number,
+    IDusuario: number,
+    IDestado: number,
+    tareanombre: string,
+    tareadescripcion: string,
+    tareaproyecto: string,
+    tareafechaf: string
+}
---
+++ src/app/services/tarea.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tareas } from '../models/tareas';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+
+  url = 'http://localhost:5000/api/tareas';
+
+  constructor(private http: HttpClient) { }
+
+  // Traer las tareas:
+  //http://localhost:5000/api/tareas
+  getTareasAll(){
+    return this.http.get(this.url)
+  }
+
+  getTareascancel(){
+    return this.http.get(`${this.url}/cancel`)
+  }
+
+  // Registrar:
+  //http://localhost:5000/api/tareas
+  postTarea(tarea: tareas){
+    return this.http.post(this.url, tarea);
+  }
+
+  // Eliminar tarea:
+  //http://localhost:5000/api/tareas
+  deleteTarea(IDtareas: any){
+    return this.http.delete(`${this.url}?IDtareas=${IDtareas}`);
+  }
+
+
+}
