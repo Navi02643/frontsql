087abd94ac77ec0726660da325c3c85828f2e449
Carlos Ivan Mercado Marin <karlos123jun@gmail.com>
2022-03-29 15:31:35 -0600

    mensaje

--- src/app/components/tareas/registrar-tarea/registrar-tarea.component.ts
+++ src/app/components/tareas/registrar-tarea/registrar-tarea.component.ts
@@ -4,6 +4,7 @@ import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
 import {NgForm} from '@angular/forms';
 import Swal from 'sweetalert2';
+import { ProjectService } from 'src/app/services/project.service';
 
 @Component({
   selector: 'app-registrar-tarea',
@@ -13,20 +14,30 @@ import Swal from 'sweetalert2';
 export class RegistrarTareaComponent implements OnInit {
 
   usuarios: any = [];
+  proyectos: any = [];
 
-  constructor( 
-    private tareasService: TareasService, 
+  constructor(
+    private tareasService: TareasService,
     private userService: UserService,
+    private proyectoService: ProjectService,
     private router: Router
     ) { }
 
   ngOnInit(): void {
+    this.allUsers();
+    this.allprojects();
   }
 
   allUsers() {
     this.userService.getuserall().subscribe((value) => {
       this.usuarios = value;
     });
+  }
+
+  allprojects(){
+    this.proyectoService.getProyectoAll().subscribe(value=>{
+      this.proyectos = value;
+    })
   }
 
   registrarTarea(form: {value: any;}):void {
